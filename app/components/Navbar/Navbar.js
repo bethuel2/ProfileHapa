d473c58480658bb42a564bc1e39848a6cf1d1638
bethuel2 <133889286+bethuel2@users.noreply.github.com>
2024-04-03 13:40:48 +0300

    Update Navbar.js

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -6,12 +6,12 @@ import { FaPhoneVolume,FaHandshakeAngle  } from "react-icons/fa6";
 
 function Navbar() {
   return (
-    <nav className='bg-black w-full p-2 md:p-6'>
+    <nav className='bg-black w-full p-2 md:p-1'>
       <div className='flex flex-col items-left md: md:mr-0'> 
       
         <p className='  text-white text-2xl hover:text-green-500'> Welcome to Horn Of Africa Peace Ambasadors</p>
       </div>
-        <ul className='flex flex-col md:flex-row gap-2 md:gap-4 justify-end md: md:mr-0'>
+        <ul className='flex flex-row md:flex-row gap-2 md:gap-4 justify-end md: md:mr-0'>
             <li> <Link href='#home' className='text-white hover:text-green-500'> <FaHome size={30} className= "text-white" />Home</Link></li>
             <li><Link href='#about us'className='text-white hover:text-green-500'> <FaInfo size={30} className= " text-white" />About Us</Link></li>
             <li> <Link href='#our team'className='text-white hover:text-green-500'><GiTeamIdea size={30} className= " text-white" />Our Team</Link></li>
